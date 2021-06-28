2c674166bc1e481289ff4344abd2920e5fd41aff
NaraPG <naragamez1@gmail.com>
2021-06-28 09:46:29 -0500

    Archivos del curso de backend con node.js

---
+++ learning-node/streams/readable-stream.js
@@ -0,0 +1,7 @@
+COLAPSARconst { Readable } = require('stream');
+const readableStream = new Readable();
+
+readableStream.push(`${0/0} `.repeat(10).concat("Batman, Batman!"));
+readableStream.push(null);
+
+readableStream.pipe(process.stdout);
